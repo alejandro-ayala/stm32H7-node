5062eff802c9fba9c95864d88995f5b2e9fddf65
alejandro.ayala@alumnos.upm.es <99070698+alejandro-ayala@users.noreply.github.com>
2024-08-14 19:16:54 +0200

    Edge detector working

--- Core/business_logic/DataHandling/ImageCapturer/EdgeDetector.cpp
+++ Core/business_logic/DataHandling/ImageCapturer/EdgeDetector.cpp
@@ -4,7 +4,17 @@ namespace business_logic
 {
 namespace DataHandling
 {
+static const std::array<std::array<int, 3>, 3> sobel_x = {{
+    {{-1, 0, 1}},
+    {{-2, 0, 2}},
+    {{-1, 0, 1}}
+}};
 
+static const std::array<std::array<int, 3>, 3> sobel_y = {{
+    {{-1, -2, -1}},
+    {{0, 0, 0}},
+    {{1, 2, 1}}
+}};
 EdgeDetector::EdgeDetector(const std::shared_ptr<SobelEdgeDetectorAlgorithm> &detectionEdgeAlgo) : m_detectionEdgeAlgo(detectionEdgeAlgo)
 {
 //    std::vector<std::pair<std::string, std::string>> filesToTest{{"test", ".jpg"}, {"test1", ".jpg"}, {"test2", ".jpg"}, {"test3", ".jpg"}, {"test4", ".jpg"}};
@@ -30,5 +40,61 @@ void EdgeDetector::processImage(const std::array<std::array<uint8_t, imgWidth>, 
      m_detectionEdgeAlgo->process(rawImage, edges);
 }
 
+void sobelGradient(const  uint8_t* image, uint8_t* edges);
+int applyKernel(const std::array<std::array<int, 3>, 3> &kernel, const std::array<std::array<int, 3>, 3> &region);
+
+void EdgeDetector::detectEdges(const  uint8_t* image, size_t size)
+{
+	uint8_t*  edges = new uint8_t[imgWidth * imgHeight];
+	sobelGradient(image, edges);
+
+}
+
+
+void sobelGradient(const  uint8_t* image, uint8_t*  edges)
+{
+    for (size_t i = 1; i < imgHeight - 1; ++i)
+    {
+        for (size_t j = 1; j < imgWidth - 1; ++j)
+        {
+            // Crear una región 3x3 alrededor del píxel actual
+            const std::array<std::array<int, 3>, 3> region = {{
+                {image[(i - 1) * imgWidth + (j - 1)], image[(i - 1) * imgWidth + j], image[(i - 1) * imgWidth + (j + 1)]},
+                {image[i * imgWidth + (j - 1)], image[i * imgWidth + j], image[i * imgWidth + (j + 1)]},
+                {image[(i + 1) * imgWidth + (j - 1)], image[(i + 1) * imgWidth + j], image[(i + 1) * imgWidth + (j + 1)]}
+            }};
+            // Calcular gradientes en dirección x e y
+            int gx = applyKernel(sobel_x, region);
+            int gy = applyKernel(sobel_y, region);
+
+            // Calcular la magnitud del gradiente
+            //edges[i][j] = sqrt(pow(gx, 2) + pow(gy, 2));
+            const auto val = sqrt(pow(gx, 2) + pow(gy, 2));
+            const auto val2 = std::min(255, static_cast<int>(sqrt(pow(gx, 2) + pow(gy, 2))));
+            edges[i * imgWidth + j] = val2;//sqrt(pow(gx, 2) + pow(gy, 2));
+        }
+    }
+}
+
+int applyKernel(const std::array<std::array<int, 3>, 3> &kernel, const std::array<std::array<int, 3>, 3> &region)
+{
+    int sum = 0;
+    const auto a = kernel.size();
+    const auto b = kernel[0].size();
+
+    for (size_t i = 0; i < a; ++i)
+    {
+        for (size_t j = 0; j < b; ++j)
+        {
+        	auto size = kernel.size();
+        	auto size2 = kernel.at(i).size();
+        	const auto reg = region.at(i).at(j);
+        	const auto ker = kernel.at(i).at(j);
+
+            sum += ker * reg;
+        }
+    }
+    return sum;
+}
 }
 }
--- Core/business_logic/DataHandling/ImageCapturer/ImageCapturer.h
+++ Core/business_logic/DataHandling/ImageCapturer/ImageCapturer.h
@@ -26,7 +26,7 @@ private:
 	void arrangeBuffer(const unsigned char *image, std::array<std::array<uint8_t, imgWidth>, imgHeight>& rawImage);
 	uint8_t* m_pic;
 	ImageStateHandle imageState;
-
+	size_t m_picSize;
 public:
 	explicit ImageCapturer(const std::shared_ptr<hardware_abstraction::Devices::ICameraDevice>& cameraControl, const std::shared_ptr<business_logic::Osal::QueueHandler> capturesQueue = nullptr);
 	virtual ~ImageCapturer();
@@ -38,6 +38,9 @@ public:
 	void decodeJPEG(uint8_t *image_buffer, uint16_t buffer_length,uint8_t greyscale);
 	//void decodeJPEG(const uint8_t *image_buffer, uint16_t buffer_length, uint8_t greyscale);
 	void encodeJPEG(uint8_t **image_buffer, unsigned long *image_size, uint8_t image_quality);
+	size_t getRawImageBufferSize() const;
+	const uint8_t* getRawImageBuffer() const;
+	void processEdges(const uint8_t* image, size_t size);
 };
 }
 }
